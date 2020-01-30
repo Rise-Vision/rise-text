abce34b4871827674689faac9e60b724e9bcfc00
Francisco Vallarino <fjvallarino@gmail.com>
2020-01-30 20:18:41 +0100

    Add support for fontsize property

--- src/rise-text.js
+++ src/rise-text.js
@@ -1,11 +1,15 @@
 import { html } from "@polymer/polymer";
+import "@polymer/polymer/lib/elements/dom-if.js";
 import { RiseElement } from "rise-common-component/src/rise-element.js";
 import { version } from "./rise-text-version.js";
 
 export default class RiseText extends RiseElement {
 
   static get template() {
-    return html`[[value]]`;
+    return html`
+      <template is="dom-if" if="{{fontsize > 0}}"><span style="font-size: [[fontsize]]px">[[value]]</span></template>
+      <template is="dom-if" if="{{!fontsize}}">[[value]]</template>
+    `;
   }
 
   static get properties() {
@@ -13,8 +17,11 @@ export default class RiseText extends RiseElement {
       value: {
         type: String,
         observer: "_valueChanged"
+      },
+      fontsize: {
+        type: Number
       }
-    }
+    };
   }
 
   // Event name constants
