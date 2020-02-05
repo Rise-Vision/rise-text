f0bf2a2247fa369621c83026caf65f0748bfecb1
Francisco Vallarino <fjvallarino@users.noreply.github.com>
2020-02-05 19:52:34 +0100

    Add support for fontsize property (#24)
    * Add support for fontsize property
    
    * Consider negative values
    
    * Use regular attribute instead of component property
    
    * Add unit tests
    
    * Initialize validFont. Send update event when fontsize changes, and include it as a value
    
    * Bump version

--- src/rise-text.js
+++ src/rise-text.js
@@ -1,11 +1,15 @@
 import { html } from "@polymer/polymer";
+import "@polymer/polymer/lib/elements/dom-if.js";
 import { RiseElement } from "rise-common-component/src/rise-element.js";
 import { version } from "./rise-text-version.js";
 
 export default class RiseText extends RiseElement {
 
   static get template() {
-    return html`[[value]]`;
+    return html`
+      <template is="dom-if" if="{{validFont}}"><span style="font-size: [[fontsize]]px">[[value]]</span></template>
+      <template is="dom-if" if="{{!validFont}}">[[value]]</template>
+    `;
   }
 
   static get properties() {
@@ -13,8 +17,12 @@ export default class RiseText extends RiseElement {
       value: {
         type: String,
         observer: "_valueChanged"
+      },
+      fontsize: {
+        type: Number,
+        observer: "_fontsizeChanged"
       }
-    }
+    };
   }
 
   // Event name constants
@@ -26,10 +34,17 @@ export default class RiseText extends RiseElement {
     super();
 
     this._setVersion( version );
+    this.validFont = false;
   }
 
   _valueChanged(newValue, oldValue) {
-    super._sendEvent( RiseText.EVENT_DATA_UPDATE, {newValue, oldValue});
+    super._sendEvent( RiseText.EVENT_DATA_UPDATE, {newValue, oldValue, fontsize: this.fontsize});
+  }
+
+  _fontsizeChanged(newValue) {
+    this.validFont = newValue && newValue > 0;
+
+    super._sendEvent( RiseText.EVENT_DATA_UPDATE, {newValue: this.value, oldValue: this.value, fontsize: this.fontsize});
   }
 }
 
